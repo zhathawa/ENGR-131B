54e64e4036012f225fffc12299f3984c8d5b7672
Zach <zach.hathaway48@gmail.com>
2019-03-19 10:42:28 -0400

    Modularizing C code

--- 20190307-PID_Lab/arduino_speak/arduino_speak.ino
+++ 20190307-PID_Lab/arduino_speak/arduino_speak.ino
@@ -1,48 +1,64 @@
-String arg1;
-String arg2;
-String arg3;
-int int_arg;
-float flt_arg;
-int count = 1;
+#define err(msg, errno) { Serial.println("%s", msg); return errno };
 
+// pointers for commands
+char* mode;
+char* cmd;
+char* opt;
+
+// various things
+int int_arg;
+int count = 1;
+int charsRead;
+
+float flt_arg;
 
 char msg[50];
 
 void setup() {
+  mode = malloc(sizeof(char) * 4);
+  if (mode == NULL)
+	err("Could not allocate memory for mode", NULL);
+
+  cmd = malloc(sizeof(char) * 4);
+  if (cmd == NULL)
+	err("Could not allocate memory for cmd", NULL);
+
+  opt = malloc(sizeof(char) * 4);
+  if (opt == NULL)
+	err("Could not allocate memory for opt", NULL);
+
   Serial.begin(115200);
 }
 
 
 void loop() {
-  char *ptr;
-  int chars;
+
+  // check that we have things to read
   if(Serial.available())
   {
     chars = Serial.readBytesUntil('\n', msg, sizeof(msg) - 1);
     msg[chars] = '\0';
-    ptr = msg;
+	
     int limit = strlen(msg);
-    for (int i = 0; i<= limit; i++){
-      if (msg[i] == '*'){
-        Serial.println(ptr);
-        if (strcmp("*IDN?", ptr) == 0){
-          Serial.println("Hello, I am your Arduino");
-          break;
-        }
-      } else if (msg[i] == ':'){
-          msg[i] = '\0';
-          Serial.println(ptr);
-          ptr = &msg[i+1];
-//          if (strcmp("M", ptr[0]) == 0)) {
-            // manual mode
-          Serial.println("Your Manual Mode Options Are...");
-//          }
-      }else{
-          Serial.println("TRYING TO MATCH *IDN?");
-          Serial.println("Not a valid command!");
-          Serial.println(ptr);
-          break;
-       }
-     }
-   } 
- }
+
+	char* fn= malloc(sizeof(char)*4);
+	fn = strncpy(fn, &fn[1], 4);
+  }
+	
+
+  // cleanup
+  freeArgs();
+}
+
+
+void* grabInput(char* msg, int position)
+{
+	: 
+}
+
+void* freeArgs()
+{
+	free(mode);
+	free(cmd);
+	free(opt);
+}
