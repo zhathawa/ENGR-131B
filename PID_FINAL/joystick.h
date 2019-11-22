1a4ecc8ebb07bd7c4ae984e5b6b4480e31de2126
zhathawa <zach.hathaway48@gmail.com>
2019-11-22 14:47:55 -0500

    Updated PID Code
    Added Joystick code.
    
    Cleaned up comments.

---
+++ PID_FINAL/joystick.h
@@ -0,0 +1,107 @@
+#ifndef JOY_H
+#define JOY_H
+
+#include "states.h"
+
+class Joystick
+{
+  private:
+    // key software stuff
+    int state;
+    int sensitivity;
+
+    float lower_limit;
+    float upper_limit;
+
+    int last_modifier;
+
+    // key hardware stuff
+    int pin;
+
+  public:
+    Joystick()
+    {
+      this->state = OFF;
+      this->sensitivity = 5;
+      this->pin = A0;
+
+      this->lower_limit = (1. / 5.) * 1023.;
+      this->upper_limit = (4. / 5.) * 1023.;
+    }
+
+    Joystick(int sensitivity)
+    {
+      this->state = OFF;
+      this->sensitivity = sensitivity;
+      this->pin = A0;
+      this->lower_limit = 1. / 5. * 1023.;
+      this->upper_limit = 4. / 5. * 1023.;
+    }
+
+    Joystick(int state, int sensitivity)
+    {
+      this->state = state;
+      this->sensitivity = sensitivity;
+      this->pin = A0;
+      this->lower_limit = 1. / 5. * 1023.;
+      this->upper_limit = 4. / 5. * 1023.;
+    }
+
+    ~Joystick(){};
+
+    // getters / setters
+    int get_state() {return this->state;}
+    void set_state(int state) {this->state = state;}
+
+    int get_sensitivity() {return this->sensitivity;}
+    void set_sensitivity(int sensitivity) {this->sensitivity = sensitivity;}
+
+    void info()
+    {
+      Serial.print("State: ");
+      Serial.print(this->state);
+      Serial.print(" || ");
+      Serial.print("Sensitivity: ");
+      Serial.println(this->sensitivity);
+    }
+
+    int directions()
+    {
+      // update voltage
+      int val = analogRead(14);
+
+      if (val > this->upper_limit)
+      {
+        return UP;
+      }
+      else if (val < this->lower_limit)
+      {
+        return DOWN;
+      }
+      else
+      {
+        return NEWT;
+      }
+    }
+
+
+// migrate timnig code to Servo
+    int modifier()
+    {
+      return this->sensitivity * directions();
+    }
+
+    int joy_run(int servoAngle) {
+      int modifier = this->modifier();
+      if (this->last_modifier != modifier) {
+        this->last_modifier = modifier;
+      }
+
+      if (modifier == NEWT)
+        return servoAngle;
+
+      return servoAngle + modifier;
+    }
+};
+
+#endif
---
+++ PID_FINAL/states.h
@@ -0,0 +1,7 @@
+#ifndef STATES_H
+#define STATES_H
+
+enum states { OFF, ON };
+enum dir_alias { UP=-1, NEWT, DOWN };
+
+#endif
